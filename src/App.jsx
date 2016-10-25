5e821fb3eb888bdd86f8d47fd08bebd66117dbcc
emyarod <allllaboutyou@gmail.com>
2016-10-24 21:41:33 -0700

    Remove boilerplate template

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,16 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.scss';
 import NoteForm from './components/notes/NoteForm';
 import Notes from './components/notes/Layout';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      speed: 10,
-    };
-  }
-
-  componentDidMount() {
-    const rootRef = firebase.database().ref('react');
-    const speedRef = rootRef.child('speed');
-    speedRef.on('value', snap => {
-      this.setState({
-        speed: snap.val(),
-      });
-    });
-  }
-
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
         <NoteForm />
         <Notes />
-        <h1>{this.state.speed}</h1>
       </div>
     );
   }
