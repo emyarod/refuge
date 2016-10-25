8f6f640815ad039e7d1bbfdb0c0d7dee9142ec02
emyarod <allllaboutyou@gmail.com>
2016-10-24 21:44:11 -0700

    Utilize stateless functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
+import React from 'react';
 // import logo from './logo.svg';
 import './App.scss';
 import NoteForm from './components/notes/NoteForm';
 import Notes from './components/notes/Layout';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <NoteForm />
-        <Notes />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <NoteForm />
+      <Notes />
+    </div>
+  );
 }
-
-export default App;
