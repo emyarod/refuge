c760bd9c8d6c22581ed5c6c10d46976891df1b7c
emyarod <allllaboutyou@gmail.com>
2016-10-24 19:17:13 -0700

    Replace index-based key with database entry ID Remove 'index as a key' anti-pattern

--- src/components/notes/Layout.jsx
+++ src/components/notes/Layout.jsx
@@ -16,10 +16,9 @@ export default class Layout extends React.Component {
     firebase.initializeApp(config);
     const notesRef = firebase.database().ref('notes');
     notesRef.on('child_added', snapshot => {
-      // name object key after note title, assign note content to keyvalue
-      const { content, title } = snapshot.val();
-      const note = {};
-      note[title] = content;
+      const { title, content } = snapshot.val();
+      const id = snapshot.W.path.o.join('');
+      const note = { title, content, id };
 
       // update state
       const newState = this.state.notes;
@@ -31,11 +30,8 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div className="notes">
-        {this.state.notes.map((note, index) => {
-          const [title] = Object.keys(note);
-          return (
-            <Note key={`note${index}`} title={title} content={note[title]} />
-          );
+        {this.state.notes.map(({ title, content, id }) => {
+          return <Note key={id} title={title} content={content} />;
         })}
       </div>
     );
