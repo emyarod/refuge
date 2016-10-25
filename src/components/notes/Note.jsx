1321b54d709a1b47092fe40f477f609ec543717c
emyarod <allllaboutyou@gmail.com>
2016-10-25 16:37:53 -0700

    Return to ES6 class

--- src/components/notes/Note.jsx
+++ src/components/notes/Note.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import './Note.scss';
 import noteRepository from '../../data/NoteRepository';
 
-export default function Note({ title, content }) {
-  return (
-    <div className="note">
-      <h1>{title}</h1>
-      <pre>{content}</pre>
-    </div>
-  );
+export default class Note extends React.Component {
+  remove() {
+    noteRepository.remove(this.props, err => {
+      // TODO: inform user
+      if (err) throw err;
+    });
+  }
+
+  render() {
+    // console.log(this.props);
+    return (
+      <div className="note">
+        <h1>{this.props.title}</h1>
+        <pre>{this.props.content}</pre>
+        <button type="button" onClick={() => this.remove()}>
+          <i className="fa fa-trash-o" aria-hidden></i>
+        </button>
+        <button className="edit" type="button">
+          <i className="fa fa-pencil" aria-hidden></i>
+        </button>
+      </div>
+    );
+  }
 }
