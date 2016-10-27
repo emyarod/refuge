eab977c07ca46e84e88f8933e9a33aadd992d904
emyarod <allllaboutyou@gmail.com>
2016-10-27 00:08:28 -0700

    Pull modal state to App layer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,47 @@ import React from 'react';
 import './App.scss';
 import NoteForm from './components/notes/NoteForm';
 import Notes from './components/notes/Layout';
+import UpdateModal from './components/notes/UpdateModal';
+import './components/notes/UpdateModal.scss';
 
-export default function App() {
-  return (
-    <div>
-      <NoteForm />
-      <Notes />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      modal: false,
+    };
+  }
+
+  dismissModal() {
+    this.setState({ modal: false, });
+  }
+
+  displayModal(note) {
+    this.setState({ modal: true, note, });
+  }
+
+  render() {
+    return (
+      <div>
+        <NoteForm />
+        <Notes modalHandler={note => this.displayModal(note)} />
+        {this.state.modal
+          ? <UpdateModal
+              note={this.state.note}
+              onDismiss={() => this.dismissModal()}
+            />
+          : null}
+      </div>
+    );
+  }
 }
+
+// export default function App() {
+//   return (
+//     <div>
+//       <NoteForm />
+//       <Notes />
+//       <UpdateModal note={{ title: 'test', content: 'lorem ipsum' }} show />
+//     </div>
+//   );
+// }
