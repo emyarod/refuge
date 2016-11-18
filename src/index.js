de7bb3598fc9813fc2ee271e738f75bfff04d344
emyarod <allllaboutyou@gmail.com>
2016-11-18 00:20:11 -0800

    Move router to App layer

--- src/index.js
+++ src/index.js
@@ -1,57 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Match, Redirect } from 'react-router';
-import App from './App';
-import Notes from './components/pages/Notes';
-import Login from './components/pages/Login';
-import Auth from './data/Auth.js';
+import Router from './App';
 import './index.scss';
 
-const MatchWhenAuthorized = ({ component: Component, ...rest }) => (
-  <Match
-    {...rest}
-    render={props => (
-      Auth.isAuthenticated
-      ? <Component {...props} />
-      : (
-        <Redirect
-          to={{
-            pathname: '/',
-            state: { from: props.location },
-          }}
-        />
-      )
-    )}
-  />
-);
-
-const Router = () => (
-  <BrowserRouter>
-    {({ router }) => (
-      <div>
-        {Auth.isAuthenticated ? (
-          <p>
-            {/* TODO: add headerbar */}
-            Headerbar! {' '}
-            <button onClick={() => (
-              Auth.signOut().then(() => router.transitionTo('/'))
-            )}>Sign out</button>
-          </p>
-        ) : (
-          <div>
-            {/* TODO: possibly remove App */}
-            <App />
-            <Login />
-          </div>
-        )}
-
-        <MatchWhenAuthorized pattern="/notes" component={Notes}/>
-      </div>
-    )}
-  </BrowserRouter>
-)
-
-ReactDOM.render(
-  <Router />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Router />, document.getElementById('root'));
